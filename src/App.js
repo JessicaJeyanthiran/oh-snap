f61db410181904c60034876753b7ff11c06ce026
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-02-16 16:25:00 -0500

    Nav component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from './components/About';
+import Nav from './components/Nav'
 import './App.css';
 
 function App() {
 
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
