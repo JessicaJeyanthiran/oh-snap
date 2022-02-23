c19b704767a745c689cdb9c24b1530be9f970706
Jessica Jeya <jeyanthiran.jessica@gmail.com>
2022-02-23 12:58:28 -0500

    gallery hook create, start of photo list function

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,47 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect} from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-    const categories = [
-        { name: "commercial", description: "Photos of grocery stores, food trucks, and other commercial projects",},
-        { name: "portraits", description: "Portraits of people in my life" },
-        { name: "food", description: "Delicious delicacies" },
-        { name: "landscape", description: "Fields, farmhouses, waterfalls, and the beauty of nature",},
-      ];
-
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-      }
+function Nav(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+      } = props;
 
   return (
-    <header>
+    <header className="flex-row px-1">
         <h2>
-            <a href="/">
-            <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+            <a data-testid="link" href="/">
+            <span role="img" aria-label="camera"> 
+            {""}
+            📸
+            </span>{""}
+            Oh Snap!
             </a>
         </h2>
         <nav>
             <ul className="flex-row">
-            <li className="mx-2">
-                <a href="#about">
-                About me
-                </a>
-            </li>
-            <li>
-                <span>Contact</span>
-            </li>
-            {categories.map((category) => (
-                <li
-                className="mx-1"
-                key={category.name}
-                >
-                <span onClick={() => categorySelected(category.name)} >
-                    {category.name}
-                </span>
+                <li className="mx-2">
+                    <a href="#about">
+                    About me
+                    </a>
                 </li>
-            ))}
+                <li className="mx-2">
+                <span>Contact</span>
+                </li>
+                {categories.map((category) => (
+                    <li className={`mx-1 ${
+                        currentCategory.name === category.name && 'navActive'
+                        }`} key={category.name}>
+                    <span
+                        onClick={() => {
+                        setCurrentCategory(category)
+                        }}
+                    >
+                        {capitalizeFirstLetter(category.name)}
+                    </span>
+                    </li>
+                ))}
             </ul>
         </nav>
-</header>
+    </header>
   );
 }
 
